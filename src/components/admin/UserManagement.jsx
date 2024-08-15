28e2d3f71579f46ad2c973fa9f04f0028bfba2bc
jubisse <jubisse@hotmail.com>
2024-08-15 03:04:03 +0200

    dashboard info

--- src/components/admin/UserManagement.jsx
+++ src/components/admin/UserManagement.jsx
@@ -7,7 +7,9 @@ export const UserManagement = () => {
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const response = await axios.get("http://localhost:3033/api/users");
+        const response = await axios.get(
+          "http://localhost:3033/api/auth/users"
+        );
         setUsers(response.data);
       } catch (error) {
         console.error("Error fetching users:", error);
